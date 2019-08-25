2283034c74a5c92bfd01982fd7968b6780bb6794
Brandon LeBlanc <brandon@leblanc.codes>
2019-08-25 14:35:44 -0500

    failing test

---
+++ tests/unit/test_cases/loops/while_else.py
@@ -0,0 +1,12 @@
+while x in y:
+    print('x in y')
+else:
+    print('x not in y')
+while x in y:
+    if x or y:
+        print("x or y")
+    else:
+        print("not x and y")
+    print("x y")
+else:
+    print("all x in y")
